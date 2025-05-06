e0eca97f01c34870abe1f6f6c552afc32768f8f0
fjnkt98 <fjnkt98@gmail.com>
2025-05-06 19:18:46 +0900

    Refactor output handling and add test for input/output

--- src/main.zig
+++ src/main.zig
@@ -2,16 +2,47 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdin = std.io.getStdIn().reader();
+    var bw = std.io.bufferedWriter(std.io.getStdOut().writer());
+    const stdout = bw.writer();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const a = gpa.allocator();
-    try run(a, stdin);
+
+    try run(a, stdin, stdout);
+
+    try bw.flush();
 }
 
-fn run(allocator: std.mem.Allocator, input: anytype) !void {
-    while (try input.readUntilDelimiterOrEofAlloc(allocator, '\n', 1073741824)) |line| {
+fn run(allocator: std.mem.Allocator, reader: anytype, writer: anytype) !void {
+    while (try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1073741824)) |line| {
         defer allocator.free(line);
 
-        std.debug.print("line: {s}\n", .{line});
+        try std.fmt.format(writer, "line: {s}\n", .{line});
     }
+}
+
+test "input and output" {
+    const allocator = std.testing.allocator;
+
+    const buffer =
+        \\foo
+        \\bar baz
+        \\qux
+    ;
+    var stream = std.io.FixedBufferStream([]const u8){ .buffer = std.mem.sliceTo(buffer, 0), .pos = 0 };
+    const reader = stream.reader();
+
+    var output = try std.ArrayList(u8).initCapacity(allocator, 1024);
+    defer output.deinit();
+    const writer = output.writer();
+
+    try run(allocator, reader, writer);
+
+    const expected =
+        \\line: foo
+        \\line: bar baz
+        \\line: qux
+        \\
+    ;
+    try std.testing.expectEqualStrings(expected, output.items);
 }
